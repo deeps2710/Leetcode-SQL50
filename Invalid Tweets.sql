9011e4bfec0d80632cf075f522f7dc68c6e496b7
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-16 14:19:36 +0530

    Create query to filter tweets by content length
    Add SQL query to select tweets longer than 15 characters.

---
+++ Invalid Tweets.sql
@@ -0,0 +1,20 @@
+/*
+Input: 
+Tweets table:
++----------+-----------------------------------+
+| tweet_id | content                           |
++----------+-----------------------------------+
+| 1        | Let us Code                       |
+| 2        | More than fifteen chars are here! |
++----------+-----------------------------------+
+Output: 
++----------+
+| tweet_id |
++----------+
+| 2        |
++----------+
+*/
+
+-- Write your MySQL query statement below
+SELECT tweet_id FROM Tweets
+WHERE LENGTH(content)>15;
