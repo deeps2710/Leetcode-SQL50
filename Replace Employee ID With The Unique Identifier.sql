0a7b159e0000cf0de070a8febbafb856a1d2f1c4
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-16 14:20:34 +0530

    Add SQL query to map Employee ID to unique_id
    Create a SQL query to replace Employee ID with unique identifiers from the EmployeeUNI table.

---
+++ Replace Employee ID With The Unique Identifier.sql
@@ -0,0 +1,37 @@
+/*
+Input: 
+Employees table:
++----+----------+
+| id | name     |
++----+----------+
+| 1  | Alice    |
+| 7  | Bob      |
+| 11 | Meir     |
+| 90 | Winston  |
+| 3  | Jonathan |
++----+----------+
+EmployeeUNI table:
++----+-----------+
+| id | unique_id |
++----+-----------+
+| 3  | 1         |
+| 11 | 2         |
+| 90 | 3         |
++----+-----------+
+Output: 
++-----------+----------+
+| unique_id | name     |
++-----------+----------+
+| null      | Alice    |
+| null      | Bob      |
+| 2         | Meir     |
+| 3         | Winston  |
+| 1         | Jonathan |
++-----------+----------+
+*/
+
+--Write your MySQL query statement below
+SELECT COALESCE(u.unique_id, NULL) AS unique_id, e.name
+FROM Employees e
+LEFT JOIN EmployeeUNI u
+ON e.id=u.id;
