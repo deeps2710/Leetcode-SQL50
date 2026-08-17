bb91cf4bdf086407b5941c3574ed3cae425e8c55
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-17 13:52:38 +0530

    Create SQL query for employee bonus retrieval
    Add SQL query to retrieve employee names and bonuses under 1000 or null.

---
+++ Employee Bonus.sql
@@ -0,0 +1,34 @@
+/*
+Input: 
+Employee table:
++-------+--------+------------+--------+
+| empId | name   | supervisor | salary |
++-------+--------+------------+--------+
+| 3     | Brad   | null       | 4000   |
+| 1     | John   | 3          | 1000   |
+| 2     | Dan    | 3          | 2000   |
+| 4     | Thomas | 3          | 4000   |
++-------+--------+------------+--------+
+Bonus table:
++-------+-------+
+| empId | bonus |
++-------+-------+
+| 2     | 500   |
+| 4     | 2000  |
++-------+-------+
+Output: 
++------+-------+
+| name | bonus |
++------+-------+
+| Brad | null  |
+| John | null  |
+| Dan  | 500   |
++------+-------+
+*/
+
+--Write your MySQL query statement below
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b
+ON e.empId=b.empId
+WHERE b.bonus<1000 OR b.bonus IS NULL;
