ad544f32cf35d99997a8d25110ef6f013d1e3348
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-17 13:48:11 +0530

    Document input and output for processing time query
    Added input and output specifications for processing time calculation.

---
+++ Average Time of Process per Machine.sql
@@ -0,0 +1,37 @@
+/*Input: 
+Activity table:
++------------+------------+---------------+-----------+
+| machine_id | process_id | activity_type | timestamp |
++------------+------------+---------------+-----------+
+| 0          | 0          | start         | 0.712     |
+| 0          | 0          | end           | 1.520     |
+| 0          | 1          | start         | 3.140     |
+| 0          | 1          | end           | 4.120     |
+| 1          | 0          | start         | 0.550     |
+| 1          | 0          | end           | 1.550     |
+| 1          | 1          | start         | 0.430     |
+| 1          | 1          | end           | 1.420     |
+| 2          | 0          | start         | 4.100     |
+| 2          | 0          | end           | 4.512     |
+| 2          | 1          | start         | 2.500     |
+| 2          | 1          | end           | 5.000     |
++------------+------------+---------------+-----------+
+Output: 
++------------+-----------------+
+| machine_id | processing_time |
++------------+-----------------+
+| 0          | 0.894           |
+| 1          | 0.995           |
+| 2          | 1.456           |
++------------+-----------------+
+*/
+
+--Write your MySQL query statement below
+SELECT a.machine_id, ROUND(AVG(b.timestamp-a.timestamp),3) AS processing_time
+FROM Activity a
+JOIN Activity b
+    ON a.machine_id=b.machine_id
+    AND a.process_id=b.process_id
+WHERE a.activity_type='start'
+AND b.activity_type='end'
+GROUP BY a.machine_id;
