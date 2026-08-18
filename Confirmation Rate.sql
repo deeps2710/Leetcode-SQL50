f5fe7e843c9317056e79d9d43b4e7eebb55dc3b3
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-18 07:59:03 +0530

    Create SQL script for calculating confirmation rates
    Add SQL query to calculate user confirmation rates based on signups and confirmations.

---
+++ Confirmation Rate.sql
@@ -0,0 +1,47 @@
+/*
+Input: 
+Signups table:
++---------+---------------------+
+| user_id | time_stamp          |
++---------+---------------------+
+| 3       | 2020-03-21 10:16:13 |
+| 7       | 2020-01-04 13:57:59 |
+| 2       | 2020-07-29 23:09:44 |
+| 6       | 2020-12-09 10:39:37 |
++---------+---------------------+
+Confirmations table:
++---------+---------------------+-----------+
+| user_id | time_stamp          | action    |
++---------+---------------------+-----------+
+| 3       | 2021-01-06 03:30:46 | timeout   |
+| 3       | 2021-07-14 14:00:00 | timeout   |
+| 7       | 2021-06-12 11:57:29 | confirmed |
+| 7       | 2021-06-13 12:58:28 | confirmed |
+| 7       | 2021-06-14 13:59:27 | confirmed |
+| 2       | 2021-01-22 00:00:00 | confirmed |
+| 2       | 2021-02-28 23:59:59 | timeout   |
++---------+---------------------+-----------+
+Output: 
++---------+-------------------+
+| user_id | confirmation_rate |
++---------+-------------------+
+| 6       | 0.00              |
+| 3       | 0.00              |
+| 7       | 1.00              |
+| 2       | 0.50              |
++---------+-------------------+
+*/
+
+--Write your MySQL query statement below
+SELECT s.user_id, 
+    ROUND(
+        AVG(CASE 
+                WHEN c.action = 'confirmed' THEN 1
+                ELSE 0
+            END),
+        2
+    ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id=c.user_id
+GROUP BY s.user_id;
