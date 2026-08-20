13bb979df6a98ab05cb0445ed2353d35821c600e
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-20 10:14:58 +0530

    Implement immediate delivery percentage calculation
    Add query to calculate immediate delivery percentage.

---
+++ Immediate Food Delivery II.sql
@@ -0,0 +1,34 @@
+/*
+Input: 
+Delivery table:
++-------------+-------------+------------+-----------------------------+
+| delivery_id | customer_id | order_date | customer_pref_delivery_date |
++-------------+-------------+------------+-----------------------------+
+| 1           | 1           | 2019-08-01 | 2019-08-02                  |
+| 2           | 2           | 2019-08-02 | 2019-08-02                  |
+| 3           | 1           | 2019-08-11 | 2019-08-12                  |
+| 4           | 3           | 2019-08-24 | 2019-08-24                  |
+| 5           | 3           | 2019-08-21 | 2019-08-22                  |
+| 6           | 2           | 2019-08-11 | 2019-08-13                  |
+| 7           | 4           | 2019-08-09 | 2019-08-09                  |
++-------------+-------------+------------+-----------------------------+
+Output: 
++----------------------+
+| immediate_percentage |
++----------------------+
+| 50.00                |
++----------------------+
+*/
+
+--Write your MySQL query statement below
+SELECT
+    ROUND(
+        SUM(CASE WHEN order_date=customer_pref_delivery_date THEN 1 ELSE 0 END)*100.0/COUNT(*),
+        2
+    ) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
