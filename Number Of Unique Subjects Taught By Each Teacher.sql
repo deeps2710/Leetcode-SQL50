32fd45398525153c2d3852009efa977f81a34513
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-21 06:39:30 +0530

    Implement SQL query for unique subjects per teacher
    Add SQL query to count unique subjects taught by each teacher.

---
+++ Number Of Unique Subjects Taught By Each Teacher.sql
@@ -0,0 +1,27 @@
+/*
+Input: 
+Teacher table:
++------------+------------+---------+
+| teacher_id | subject_id | dept_id |
++------------+------------+---------+
+| 1          | 2          | 3       |
+| 1          | 2          | 4       |
+| 1          | 3          | 3       |
+| 2          | 1          | 1       |
+| 2          | 2          | 1       |
+| 2          | 3          | 1       |
+| 2          | 4          | 1       |
++------------+------------+---------+
+Output:  
++------------+-----+
+| teacher_id | cnt |
++------------+-----+
+| 1          | 2   |
+| 2          | 4   |
++------------+-----+
+*/
+
+--Write your MySQL query statement below
+SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt
+FROM Teacher
+GROUP BY teacher_id;
