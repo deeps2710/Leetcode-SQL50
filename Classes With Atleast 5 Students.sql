c65dd3fd019c36dca5b201810fc2dec7b1cf4942
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-21 20:21:49 +0530

    Add SQL query for classes with 5 or more students
    Query to select classes with at least 5 students from the Courses table.

---
+++ Classes With Atleast 5 Students.sql
@@ -0,0 +1,27 @@
+/*
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+*/
+
+--Write your MySQL query statement below
+SELECT class FROM Courses
+GROUP BY class HAVING COUNT(student)>=5;
