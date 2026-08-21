1d1b12381fce019c67fb28c900afac86360417ef
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-21 20:26:00 +0530

    Add SQL query to count followers for each user
    This SQL query counts the number of followers for each user and orders the results by user_id.

---
+++ Find Followers Count.sql
@@ -0,0 +1,25 @@
+/*
+Input: 
+Followers table:
++---------+-------------+
+| user_id | follower_id |
++---------+-------------+
+| 0       | 1           |
+| 1       | 0           |
+| 2       | 0           |
+| 2       | 1           |
++---------+-------------+
+Output: 
++---------+----------------+
+| user_id | followers_count|
++---------+----------------+
+| 0       | 1              |
+| 1       | 1              |
+| 2       | 2              |
++---------+----------------+
+*/
+
+--Write your MySQL query statement below
+SELECT user_id, COUNT(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id ORDER BY user_id;
