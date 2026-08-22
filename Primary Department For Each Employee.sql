59f0bb6351328456d191fd1e040ed22f1d4fbbfd
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-22 17:21:00 +0530

    Add SQL query for primary department retrieval
    This SQL query retrieves the primary department for each employee based on the primary_flag and includes employees with only one department.

---
+++ Primary Department For Each Employee.sql
@@ -0,0 +1,37 @@
+/*
+Input: 
+Employee table:
++-------------+---------------+--------------+
+| employee_id | department_id | primary_flag |
++-------------+---------------+--------------+
+| 1           | 1             | N            |
+| 2           | 1             | Y            |
+| 2           | 2             | N            |
+| 3           | 3             | N            |
+| 4           | 2             | N            |
+| 4           | 3             | Y            |
+| 4           | 4             | N            |
++-------------+---------------+--------------+
+Output: 
++-------------+---------------+
+| employee_id | department_id |
++-------------+---------------+
+| 1           | 1             |
+| 2           | 1             |
+| 3           | 3             |
+| 4           | 3             |
++-------------+---------------+
+*/
+
+--Write your MySQL query statement below
+SELECT 
+    employee_id, 
+    department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+OR employee_id IN (
+    SELECT employee_id 
+    FROM Employee 
+    GROUP BY employee_id 
+    HAVING COUNT(department_id) = 1
+);
