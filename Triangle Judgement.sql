339b2fea116007b06b18e00037e2403c6cbe958b
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-22 17:30:08 +0530

    Create Triangle Judgement SQL script
    Add SQL query to determine if triangle is valid based on side lengths.

---
+++ Triangle Judgement.sql
@@ -0,0 +1,25 @@
+/*
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+*/
+
+--Write your MySQL query statement below
+SELECT x,y,z,
+CASE
+    WHEN x+y>z AND x+z>y AND y+z>x THEN 'Yes'
+    ELSE 'No'
+END AS triangle
+FROM Triangle;
