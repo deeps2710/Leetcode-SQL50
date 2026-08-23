8e8161a3fb268c0e1b773cbc6a92aa2705275aac
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-23 11:41:48 +0530

    Implement query for product prices at specific date
    Add MySQL query to retrieve product prices based on change dates.

---
+++ Product Price At A Given Date.sql
@@ -0,0 +1,41 @@
+/*
+Input: 
+Products table:
++------------+-----------+-------------+
+| product_id | new_price | change_date |
++------------+-----------+-------------+
+| 1          | 20        | 2019-08-14  |
+| 2          | 50        | 2019-08-14  |
+| 1          | 30        | 2019-08-15  |
+| 1          | 35        | 2019-08-16  |
+| 2          | 65        | 2019-08-17  |
+| 3          | 20        | 2019-08-18  |
++------------+-----------+-------------+
+Output: 
++------------+-------+
+| product_id | price |
++------------+-------+
+| 2          | 50    |
+| 1          | 35    |
+| 3          | 10    |
++------------+-------+
+*/
+
+--Write your MySQL query statement below
+-- Part 1: Products with a price change on or before 2019-08-16
+SELECT product_id, new_price AS price
+FROM Products
+WHERE (product_id, change_date) IN (
+    SELECT product_id, MAX(change_date)
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+)
+
+UNION ALL
+
+-- Part 2: Products whose first change happened AFTER 2019-08-16
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16';
