b7940111611d648113f718752ab293fce10afb27
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-23 11:30:29 +0530

    Create SQL query for consecutive numbers
    Add SQL query to find consecutive numbers in Logs table.

---
+++ Consecutive Numbers.sql
@@ -0,0 +1,28 @@
+/*
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+*/
+
+--Write your MySQL query statement below
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 ON l1.id = l2.id - 1
+JOIN Logs l3 ON l1.id = l3.id - 2
+WHERE l1.num = l2.num AND l2.num = l3.num;
