ba5b14939140f6bab4c1538323a8ecc04e46809e
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-24 13:14:38 +0530

    Add SQL query for customer spending analysis
    This SQL script calculates the total and average amount spent by customers over a rolling 7-day period, based on their visit dates.

---
+++ Restaurant Growth.sql
@@ -0,0 +1,42 @@
+/*
+Input: 
+Customer table:
++-------------+--------------+--------------+-------------+
+| customer_id | name         | visited_on   | amount      |
++-------------+--------------+--------------+-------------+
+| 1           | Jhon         | 2019-01-01   | 100         |
+| 2           | Daniel       | 2019-01-02   | 110         |
+| 3           | Jade         | 2019-01-03   | 120         |
+| 4           | Khaled       | 2019-01-04   | 130         |
+| 5           | Winston      | 2019-01-05   | 110         | 
+| 6           | Elvis        | 2019-01-06   | 140         | 
+| 7           | Anna         | 2019-01-07   | 150         |
+| 8           | Maria        | 2019-01-08   | 80          |
+| 9           | Jaze         | 2019-01-09   | 110         | 
+| 1           | Jhon         | 2019-01-10   | 130         | 
+| 3           | Jade         | 2019-01-10   | 150         | 
++-------------+--------------+--------------+-------------+
+Output: 
++--------------+--------------+----------------+
+| visited_on   | amount       | average_amount |
++--------------+--------------+----------------+
+| 2019-01-07   | 860          | 122.86         |
+| 2019-01-08   | 840          | 120            |
+| 2019-01-09   | 840          | 120            |
+| 2019-01-10   | 1000         | 142.86         |
++--------------+--------------+----------------+
+*/
+
+--Write your MySQL query statement below
+SELECT 
+    c1.visited_on,
+    SUM(c2.amount) AS amount,
+    ROUND(SUM(c2.amount) / 7.0, 2) AS average_amount
+FROM (SELECT DISTINCT visited_on FROM Customer) c1
+JOIN Customer c2 
+    ON DATEDIFF(c1.visited_on, c2.visited_on) BETWEEN 0 AND 6
+WHERE c1.visited_on >= (
+    SELECT MIN(visited_on) FROM Customer
+) + INTERVAL 6 DAY
+GROUP BY c1.visited_on
+ORDER BY c1.visited_on;
