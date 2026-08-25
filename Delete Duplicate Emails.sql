854ddb32e41c523b4d7d904983fead795145a5d2
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-25 07:57:30 +0530

    Add SQL script to delete duplicate emails
    This SQL script deletes duplicate emails from the Person table, keeping the record with the lowest id for each email.

---
+++ Delete Duplicate Emails.sql
@@ -0,0 +1,25 @@
+/*
+Input: 
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Output: 
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+*/
+
+-- Write your MySQL query statement below
+DELETE p1
+FROM Person p1
+JOIN Person p2
+ON p1.email=p2.email
+AND p1.id>p2.id;
