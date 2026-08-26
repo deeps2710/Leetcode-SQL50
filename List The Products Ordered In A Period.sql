7f3ccfb2412216a4b8ebd60f3296f53aed55026b
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-26 07:01:00 +0530

    Add SQL query to list products ordered in February 2020
    This SQL query retrieves the names and total units of products ordered within February 2020, filtering for products with a total order quantity of at least 100.

---
+++ List The Products Ordered In A Period.sql
@@ -0,0 +1,39 @@
+/*
+Input: 
+Products table:
++-------------+-----------------------+------------------+
+| product_id  | product_name          | product_category |
++-------------+-----------------------+------------------+
+| 1           | Leetcode Solutions    | Book             |
+| 2           | Jewels of Stringology | Book             |
+| 3           | HP                    | Laptop           |
+| 4           | Lenovo                | Laptop           |
+| 5           | Leetcode Kit          | T-shirt          |
++-------------+-----------------------+------------------+
+Orders table:
++--------------+--------------+----------+
+| product_id   | order_date   | unit     |
++--------------+--------------+----------+
+| 1            | 2020-02-05   | 60       |
+| 1            | 2020-02-10   | 70       |
+| 2            | 2020-01-18   | 30       |
+| 2            | 2020-02-11   | 80       |
+| 3            | 2020-02-17   | 2        |
+| 3            | 2020-02-24   | 3        |
+| 4            | 2020-03-01   | 20       |
+| 4            | 2020-03-04   | 30       |
+| 4            | 2020-03-04   | 60       |
+| 5            | 2020-02-25   | 50       |
+| 5            | 2020-02-27   | 50       |
+| 5            | 2020-03-01   | 50       |
++--------------+--------------+----------+
+*/
+
+--Write your MySQL query statement below
+SELECT p.product_name AS product_name, SUM(o.unit) AS unit
+FROM Products p
+Join Orders o
+ON p.product_id=o.product_id
+WHERE order_date>='2020-02-01' AND order_date<='2020-02-29'
+GROUP BY p.product_id, p.product_name
+HAVING SUM(o.unit)>=100;
