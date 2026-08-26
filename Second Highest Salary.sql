60be534d1142b062cb55845ddd887ea44efb7e7f
Deepshikha Rani <deepshikharani297@gmail.com>
2026-08-26 06:57:33 +0530

    Create SQL script for second highest salary
    Add SQL query to find the second highest salary from the Employee table.

---
+++ Second Highest Salary.sql
@@ -0,0 +1,41 @@
+/*
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+*/
+
+--Write your MySQL query statement below
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary<(
+    SELECT MAX(salary) FROM Employee
+);
